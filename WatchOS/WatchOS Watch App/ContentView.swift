f306ce130183a1751bd869cc5dd50bc2be8d93a3
KUBA <rogutkuba@gmail.com>
2023-06-15 18:31:23 -0400

    adding watchOS skeleton files

---
+++ WatchOS/WatchOS Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WatchOS Watch App
+//
+//  Created by Kuba Rogut on 2023-06-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WatchOS/WatchOS Watch App/WatchOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchOSApp.swift
+//  WatchOS Watch App
+//
+//  Created by Kuba Rogut on 2023-06-15.
+//
+
+import SwiftUI
+
+@main
+struct WatchOS_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
