e1f7cd79afc489cd1d04ea29a5ad109893a7e147
Rizwan i7 <rizwankassamali1@gmail.com>
2023-07-23 23:15:46 -0400

    basic UI nav

--- iOS-mobile/TempoTracks/App.js
+++ iOS-mobile/TempoTracks/App.js
@@ -1,20 +1,139 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// React Native Bottom Navigation
+// https://aboutreact.com/react-native-bottom-navigation/
+import * as React from 'react';
 
-export default function App() {
+import
+ MaterialCommunityIcons
+from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import HomePage from './pages/HomePage';
+import SettingsPage from './pages/SettingsPage';
+import WorkoutsPage from './pages/WorkoutsPage';
+import MusicPage from './pages/MusicPage';
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function HomeStack() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{headerShown: false}}
+        >
+        <Stack.Screen
+          name="Home"
+          component={HomePage} />
+        <Stack.Screen
+          name="Workouts"
+          component={WorkoutsPage} />
+      </Stack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function SettingsStack() {
+  return (
+    <Stack.Navigator
+      initialRouteName="Settings"
+      screenOptions={{headerShown: false}}>
+      <Stack.Screen
+        name="Settings"
+        component={SettingsPage} />
+      <Stack.Screen
+        name="Music"
+        component={MusicPage} />
+    </Stack.Navigator>
+  );
+}
+
+function MusicStack() {
+  return (
+    <Stack.Navigator
+      initialRouteName="Music"
+      screenOptions={{headerShown: false}}>
+          <Stack.Screen
+          name="Music"
+          component={MusicPage} />
+    </Stack.Navigator>
+  );
+}
+
+function WorkoutsStack() {
+  return (
+    <Stack.Navigator
+      initialRouteName="Workouts"
+      screenOptions={{headerShown: false}}>
+          <Stack.Screen
+          name="Workouts"
+          component={WorkoutsPage} />
+    </Stack.Navigator>
+  );
+}
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Feed"
+        screenOptions={({ route }) => ({
+          headerStyle: { backgroundColor: '#42f44b' },
+          headerTintColor: '#fff',
+          headerTitleStyle: { fontWeight: 'bold' },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            if (route.name === 'HomeStack') {
+              iconName = focused
+                ? 'home-circle'
+                : 'home-circle-outline';
+            } else if (route.name === 'SettingsStack') {
+              iconName = focused
+                ? 'account-settings'
+                : 'account-settings-outline';
+            }
+            return (
+              <MaterialCommunityIcons
+                name={iconName}
+                size={size}
+                color={color}
+              />
+            );
+          }
+        })}>
+        <Tab.Screen
+          name="HomeStack"
+          component={HomeStack}
+          options={{
+            tabBarLabel: 'Home',
+            title: 'Home',
+          }}  />
+        <Tab.Screen
+          name="MusicStack"
+          component={MusicStack}
+          options={{
+            tabBarLabel: 'Music',
+            title: 'Music'
+          }} />
+          <Tab.Screen
+          name="WorkoutsStack"
+          component={WorkoutsStack}
+          options={{
+            tabBarLabel: 'Workouts',
+            title: 'Workouts'
+          }} />
+              <Tab.Screen
+          name="SettingsStack"
+          component={SettingsStack}
+          options={{
+            tabBarLabel: 'Settings',
+            title: 'Setting'
+          }} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+export default App;
