48ef7720244d2642ddfb8c6da95b890427f4427e
Rizwan i7 <rizwankassamali1@gmail.com>
2023-07-24 00:49:56 -0400

    clean up stuff

--- iOS-mobile/TempoTracks/pages/HomePage.js
+++ iOS-mobile/TempoTracks/pages/HomePage.js
@@ -17,7 +17,7 @@ const HomePage = ({ navigation }) => {
       style={{
         alignItems: "center",
         width: "100%",
-        height: 100,
+        height: 125,
         paddingTop: 8,
         marginBottom: 15,
       }}
@@ -44,7 +44,7 @@ const HomePage = ({ navigation }) => {
       style={{
         alignItems: "center",
         width: "100%",
-        height: 100,
+        height: 125,
         paddingTop: 8,
         marginBottom: 15,
       }}
@@ -64,90 +64,6 @@ const HomePage = ({ navigation }) => {
       </View>
     </View>
   );
-
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      flexDirection: "column",
-      justifyContent: "flex-start",
-      alignItems: "center",
-    },
-    progress: {
-      width: "95%",
-      height: 125,
-      marginTop: 15,
-      alignItems: "center",
-    },
-    title_box: {
-      backgroundColor: "#74b3ce",
-      borderRadius: 10,
-      width: "95%",
-      height: 40,
-      justifyContent: "center",
-    },
-    title: {
-      color: "rgba(255,255,255,1)",
-      fontSize: 22,
-      alignSelf: "center",
-    },
-    progress_container: {
-      flexDirection: "row",
-      width: "100%",
-      marginTop: 20,
-      justifyContent: "center",
-    },
-    progress_box: {
-      backgroundColor: "rgba(213,218,223,1)",
-      width: "40%",
-      height: 55,
-      borderRadius: 10,
-      marginHorizontal: 25,
-    },
-    progress_title: {
-      color: "#121212",
-      alignSelf: "center",
-      marginVertical: 4,
-    },
-    progress_value: {
-      color: "#121212",
-      fontSize: 20,
-      fontWeight: "bold",
-      alignSelf: "center",
-    },
-    exercise_container: {
-      width: "95%",
-      height: 400,
-      alignItems: "center",
-    },
-    box: {
-      backgroundColor: "rgba(213,218,223,1)",
-      borderRadius: 10,
-      width: "95%",
-      height: 275,
-      alignSelf: "center",
-    },
-    btn_box: {
-      flexDirection: "row",
-      justifyContent: "center",
-      margin: 30,
-      alignContent: "center",
-    },
-    btn_shape: {
-      backgroundColor: "#09bc8a",
-      borderRadius: 10,
-      width: "50%",
-      height: 40,
-      marginTop: 10,
-      justifyContent: "center",
-    },
-    btn_text: {
-      color: "rgba(255,255,255,1)",
-      fontSize: 16,
-      textAlign: "center",
-      fontWeight: "bold",
-    },
-  });
-
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
@@ -207,6 +123,89 @@ const HomePage = ({ navigation }) => {
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  progress: {
+    width: "95%",
+    height: 125,
+    marginTop: 15,
+    alignItems: "center",
+  },
+  title_box: {
+    backgroundColor: "#74b3ce",
+    borderRadius: 10,
+    width: "95%",
+    height: 40,
+    justifyContent: "center",
+  },
+  title: {
+    color: "rgba(255,255,255,1)",
+    fontSize: 22,
+    alignSelf: "center",
+  },
+  progress_container: {
+    flexDirection: "row",
+    width: "100%",
+    marginTop: 20,
+    justifyContent: "center",
+  },
+  progress_box: {
+    backgroundColor: "rgba(213,218,223,1)",
+    width: "40%",
+    height: 55,
+    borderRadius: 10,
+    marginHorizontal: 25,
+  },
+  progress_title: {
+    color: "#121212",
+    alignSelf: "center",
+    marginVertical: 4,
+  },
+  progress_value: {
+    color: "#121212",
+    fontSize: 20,
+    fontWeight: "bold",
+    alignSelf: "center",
+  },
+  exercise_container: {
+    width: "95%",
+    height: 400,
+    alignItems: "center",
+  },
+  box: {
+    backgroundColor: "rgba(213,218,223,1)",
+    borderRadius: 10,
+    width: "95%",
+    height: 275,
+    alignSelf: "center",
+  },
+  btn_box: {
+    flexDirection: "row",
+    justifyContent: "center",
+    margin: 30,
+    alignContent: "center",
+  },
+  btn_shape: {
+    backgroundColor: "#09bc8a",
+    borderRadius: 10,
+    width: "50%",
+    height: 40,
+    marginTop: 10,
+    justifyContent: "center",
+  },
+  btn_text: {
+    color: "rgba(255,255,255,1)",
+    fontSize: 16,
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+});
 
 
 export default HomePage;
