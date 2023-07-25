06fb538e8b660c02bb4e838de01e8179f53d0438
Sterlyn <sterlyntang31@gmail.com>
2023-07-25 15:34:50 -0400

    prettier formatted files; only changed the "pt" values

--- iOS-mobile/TempoTracks/src/pages/LaunchPage.js
+++ iOS-mobile/TempoTracks/src/pages/LaunchPage.js
@@ -1,78 +1,79 @@
-import * as React from 'react';
-import { View, Text, StyleSheet, Image, Pressable } from 'react-native';
+import * as React from "react";
+import { View, Text, StyleSheet, Image, Pressable } from "react-native";
 
 const LaunchPage = ({ navigation }) => {
-    return (
-        <View style={styles.full}>
-            <View style={[styles.container, { flexDirection: 'column' }]}>
-                <View style={{ flex: 10 }}>
-                  
-                </View>
-                <View style={{ flex: 10 }}>
-                    <Text style={styles.title}>TempoTracks</Text>
-                    <Text style={styles.body}>Music to inspire, Beats to motivate.</Text>
-                </View>
-                <View style={{ flex: 2 }}>
-
-                </View>
-
-            </View>
-            <View style={[styles.buttonContainer, { flexDirection: 'row' }]}>
-                <Pressable style={styles.regButton} onPress={() => navigation.navigate("Register")}>
-                    <Text style={{ fontSize: 16, fontWeight: 'bold' }}>Register</Text>
-                </Pressable>
-                <Pressable style={styles.signButton} onPress={() => navigation.navigate("SignIn")}>
-                    <Text style={{ color: 'white', fontSize: 16, fontWeight: 'bold' }}>Sign In</Text>
-                </Pressable>
-            </View>
+  return (
+    <View style={styles.full}>
+      <View style={[styles.container, { flexDirection: "column" }]}>
+        <View style={{ flex: 10 }}></View>
+        <View style={{ flex: 10 }}>
+          <Text style={styles.title}>TempoTracks</Text>
+          <Text style={styles.body}>Music to inspire, Beats to motivate.</Text>
         </View>
-
-
-    );
-}
+        <View style={{ flex: 2 }}></View>
+      </View>
+      <View style={[styles.buttonContainer, { flexDirection: "row" }]}>
+        <Pressable
+          style={styles.regButton}
+          onPress={() => navigation.navigate("Register")}
+        >
+          <Text style={{ fontSize: 16, fontWeight: "bold" }}>Register</Text>
+        </Pressable>
+        <Pressable
+          style={styles.signButton}
+          onPress={() => navigation.navigate("SignIn")}
+        >
+          <Text style={{ color: "white", fontSize: 16, fontWeight: "bold" }}>
+            Sign In
+          </Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    full: {
-        height: "100%",
-        padding: "30pt",
-        backgroundColor: "#181818"
-    },
-    container: {
-        height: "90%",
-        justifyContent: 'space-between',
-        backgroundColor: "#181818"
-    },
-    title: {
-        fontSize: "40pt",
-        alignSelf: 'center',
-        color: 'white'
-    },
-    body: {
-        fontSize: "12pt",
-        alignSelf: 'center',
-        color: '#6b6b6b'
-    },
-    buttonContainer: {
-        width: "100%",
-        height: "45pt",
-        backgroundColor: '#222222',
-        borderRadius: 20,
-        justifyContent: 'space-between',
-    },
-    regButton: {
-        borderRadius: 20,
-        backgroundColor: 'white',
-        textAlign: 'center',
-        width: "50%",
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    signButton: {
-        borderRadius: 20,
-        backgroundColor: "transparent",
-        width: "50%",
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
+  full: {
+    height: "100%",
+    padding: 30,
+    backgroundColor: "#181818",
+  },
+  container: {
+    height: "90%",
+    justifyContent: "space-between",
+    backgroundColor: "#181818",
+  },
+  title: {
+    fontSize: 40,
+    alignSelf: "center",
+    color: "white",
+  },
+  body: {
+    fontSize: 12,
+    alignSelf: "center",
+    color: "#6b6b6b",
+  },
+  buttonContainer: {
+    width: "100%",
+    height: 45,
+    backgroundColor: "#222222",
+    borderRadius: 20,
+    justifyContent: "space-between",
+  },
+  regButton: {
+    borderRadius: 20,
+    backgroundColor: "white",
+    textAlign: "center",
+    width: "50%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  signButton: {
+    borderRadius: 20,
+    backgroundColor: "transparent",
+    width: "50%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
 export default LaunchPage;
--- iOS-mobile/TempoTracks/src/pages/SignInPage.js
+++ iOS-mobile/TempoTracks/src/pages/SignInPage.js
@@ -1,141 +1,162 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, Pressable, Alert } from 'react-native';
-import { userLogIn } from '../api/User';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Pressable,
+  Alert,
+} from "react-native";
+import { userLogIn } from "../api/User";
 
 const SignInPage = ({ navigation }) => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [textColor, setTextColor] = useState('#181818');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [textColor, setTextColor] = useState("#181818");
 
-    function checkLogin() {
-        if (email == "admin" && password == "admin") {
-            navigation.navigate('Root', { screen: 'Home' })
-        } else {
-            setTextColor('#ff5555')
-        }
-
-
-        // if (username == "" || password == "") {
-        //     setTextColor('#ff5555')
-        // }
-        // else {
-        //     let data = userLogIn(username, password);
-
-        //     if (data == null) {
-        //         setTextColor('#ff5555')
-        //     }
-        //     else {
-        //         // save the userId to the global state
-        //         navigation.navigate('Root', { screen: 'Home' })
-        //     }
-        // }
+  function checkLogin() {
+    if (email == "admin" && password == "admin") {
+      navigation.navigate("Root", { screen: "Home" });
+    } else {
+      setTextColor("#ff5555");
     }
 
-    return (
-        <View style={styles.full}>
-            <View style={[styles.container, { flexDirection: 'column' }]}>
-                <View style={{ flex: 2 }}>
-                    <Pressable style={styles.signButton} onPress={() => navigation.navigate("Launch")}>
-                        <Text style={{ color: 'white', fontSize: 16, fontWeight: '100' }}>Back</Text>
-                    </Pressable>
-                </View>
-                <View style={{ flex: 6 }}>
-                    <Text style={styles.title}>Let's sign you in.</Text>
-                    <Text style={styles.body}>Welcome back.</Text>
-                    <Text style={styles.body}>You've been missed!</Text>
-                </View>
-                <View style={{ flex: 10 }}>
-                    <TextInput style={styles.input} placeholder='Username' onChangeText={(username) => setUsername(username)} />
-                    <TextInput style={styles.input} placeholder='Password' onChangeText={(password) => setPassword(password)} secureTextEntry={true} />
-                    <Text style={[styles.help2, { color: textColor }]}>Incorrect Username/Password.</Text>
-                </View>
-                <View style={{ flex: 1 }}>
-                    <View style={[styles.helpContainer, { flexDirection: 'row' }]}>
-                        <Text style={styles.help}>Don't have an account? </Text>
-                        <Text style={styles.help1}>Register</Text>
-                    </View>
-                </View>
-            </View>
-            <View>
-                <Pressable style={styles.buttonContainer} onPress={checkLogin}>
-                    <Text style={{ fontSize: 16, fontWeight: 'bold' }}>Sign In</Text>
-                </Pressable>
-            </View>
-        </View>
+    // if (username == "" || password == "") {
+    //     setTextColor('#ff5555')
+    // }
+    // else {
+    //     let data = userLogIn(username, password);
 
-    );
-}
+    //     if (data == null) {
+    //         setTextColor('#ff5555')
+    //     }
+    //     else {
+    //         // save the userId to the global state
+    //         navigation.navigate('Root', { screen: 'Home' })
+    //     }
+    // }
+  }
+
+  return (
+    <View style={styles.full}>
+      <View style={[styles.container, { flexDirection: "column" }]}>
+        <View style={{ flex: 2 }}>
+          <Pressable
+            style={styles.signButton}
+            onPress={() => navigation.navigate("Launch")}
+          >
+            <Text style={{ color: "white", fontSize: 16, fontWeight: "100" }}>
+              Back
+            </Text>
+          </Pressable>
+        </View>
+        <View style={{ flex: 6 }}>
+          <Text style={styles.title}>Let's sign you in.</Text>
+          <Text style={styles.body}>Welcome back.</Text>
+          <Text style={styles.body}>You've been missed!</Text>
+        </View>
+        <View style={{ flex: 10 }}>
+          <TextInput
+            style={styles.input}
+            placeholder="Username"
+            onChangeText={(username) => setUsername(username)}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            onChangeText={(password) => setPassword(password)}
+            secureTextEntry={true}
+          />
+          <Text style={[styles.help2, { color: textColor }]}>
+            Incorrect Username/Password.
+          </Text>
+        </View>
+        <View style={{ flex: 1 }}>
+          <View style={[styles.helpContainer, { flexDirection: "row" }]}>
+            <Text style={styles.help}>Don't have an account? </Text>
+            <Text style={styles.help1}>Register</Text>
+          </View>
+        </View>
+      </View>
+      <View>
+        <Pressable style={styles.buttonContainer} onPress={checkLogin}>
+          <Text style={{ fontSize: 16, fontWeight: "bold" }}>Sign In</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    full: {
-        height: "100%",
-        padding: "30pt",
-        backgroundColor: "#181818"
-    },
-    container: {
-        height: "90%",
-        justifyContent: 'space-between',
-        backgroundColor: "#181818"
-    },
-    helpContainer: {
-        alignSelf: 'center',
-    },
-    title: {
-        fontSize: "26pt",
-        color: 'white',
-        paddingBottom: 20
-    },
-    body: {
-        fontSize: "26pt",
-        color: 'white',
-        fontWeight: 100
-    },
-    buttonContainer: {
-        width: "100%",
-        height: "45pt",
-        justifyContent: 'space-between',
-        borderRadius: 20,
-        backgroundColor: "white",
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    help: {
-        alignSelf: 'center',
-        color: '#6b6b6b',
-        fontSize: "11pt"
-    },
-    help1: {
-        alignSelf: 'center',
-        color: 'white',
-        fontSize: "11pt",
-    },
-    help2: {
-        color: '#6b6b6b',
-        fontSize: "11pt",
-        marginLeft: 9,
-        marginTop: 4
-    },
-    help3: {
-        alignSelf: 'center',
-        color: 'white',
-        fontSize: "11pt",
-        marginTop: 4
-    },
-    input: {
-        width: "100%",
-        height: "45pt",
-        margin: 5,
-        borderRadius: 20,
-        borderWidth: 2,
-        borderColor: '#3b3b3b',
-        backgroundColor: "#222222",
-        alignItems: 'center',
-        justifyContent: 'center',
-        placeholderTextColor: '#3b3b3b',
-        padding: 15,
-        fontWeight: 500,
-        color: 'white',
-        fontSize: 15
-    }
+  full: {
+    height: "100%",
+    padding: 30,
+    backgroundColor: "#181818",
+  },
+  container: {
+    height: "90%",
+    justifyContent: "space-between",
+    backgroundColor: "#181818",
+  },
+  helpContainer: {
+    alignSelf: "center",
+  },
+  title: {
+    fontSize: 26,
+    color: "white",
+    paddingBottom: 20,
+  },
+  body: {
+    fontSize: 26,
+    color: "white",
+    fontWeight: 100,
+  },
+  buttonContainer: {
+    width: "100%",
+    height: 45,
+    justifyContent: "space-between",
+    borderRadius: 20,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  help: {
+    alignSelf: "center",
+    color: "#6b6b6b",
+    fontSize: 11,
+  },
+  help1: {
+    alignSelf: "center",
+    color: "white",
+    fontSize: 11,
+  },
+  help2: {
+    color: "#6b6b6b",
+    fontSize: 11,
+    marginLeft: 9,
+    marginTop: 4,
+  },
+  help3: {
+    alignSelf: "center",
+    color: "white",
+    fontSize: 11,
+    marginTop: 4,
+  },
+  input: {
+    width: "100%",
+    height: 45,
+    margin: 5,
+    borderRadius: 20,
+    borderWidth: 2,
+    borderColor: "#3b3b3b",
+    backgroundColor: "#222222",
+    alignItems: "center",
+    justifyContent: "center",
+    placeholderTextColor: "#3b3b3b",
+    padding: 15,
+    fontWeight: "500",
+    color: "white",
+    fontSize: 15,
+  },
 });
 export default SignInPage;
