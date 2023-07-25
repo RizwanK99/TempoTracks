b7748dfa036544d36d511c05419bae5b189430ef
KUBA <rogutkuba@gmail.com>
2023-07-25 19:13:44 -0400

    working speeding up/down music

--- iOS-mobile/TempoTracks/ios/Counter.m
+++ iOS-mobile/TempoTracks/ios/Counter.m
@@ -18,6 +18,21 @@ RCT_EXTERN_METHOD(
   requestAuthorization: (RCTPromiseResolveBlock)resolve
   rejecter: (RCTPromiseRejectBlock)reject
 )
+RCT_EXTERN_METHOD(
+  getSongLibrary: (RCTPromiseResolveBlock)resolve
+  rejecter: (RCTPromiseRejectBlock)reject
+)
+RCT_EXTERN_METHOD(
+  playSongWithId: (NSString)songId
+)
+RCT_EXTERN_METHOD(
+  changePlayerPlayback: (NSString)nextState
+)
+RCT_EXTERN_METHOD(
+  changePlayerState: (NSNumber)playbackRate
+  repeat: (NSString)repeatMode
+  shuffle: (NSString)shuffleMode
+)
 
 RCT_EXTERN_METHOD(getCount: (RCTResponseSenderBlock)callback)
 
--- iOS-mobile/TempoTracks/src/pages/PocPage.js
+++ iOS-mobile/TempoTracks/src/pages/PocPage.js
@@ -1,4 +1,5 @@
-import {
+import { useEffect, useState } from 'react'
+import { 
   TouchableOpacity,
   StyleSheet,
   View,
@@ -8,33 +9,87 @@ import {
   ScrollView
 } from 'react-native';
 import { NativeModules } from 'react-native'
+// import Slider from '@react-native-community/slider';
 
 const PocPage = () => {
-  const getCountFunc = () => {
-    NativeModules.Counter.getCount(value => {
-      console.log("count is " + value)
-    })
-  }
 
-  const getDecrementFunc = () => {
-    NativeModules.Counter.decrement()
-      .then(res => console.log(res))
-      .catch(e => console.log(e.message, e.code))
-  }
+  const [songs, setSongs] = useState([])
 
-  const requestAuth = () => {
+  useEffect(() => {
+    console.log("fetching")
+    requestAuth()
+  }, [])
+
+  const requestAuth = async () => {
     NativeModules.Counter.requestAuthorization()
-      .then(res => console.log(res))
+      .then(res => {
+        console.log("auth response", res)
+        getSongLibrary()
+      })
       .catch(e => console.log(e.message, e.code))
+  }
+
+  const getSongLibrary = () => {
+    NativeModules.Counter.getSongLibrary()
+      .then(res => {
+        console.log("songs", res[0])
+        setSongs(res[0])
+      })
+      .catch(e => console.log(e.message, e.code))
+  }
+
+  const playSong = (songId) => {
+    NativeModules.Counter.playSongWithId(songId)
+  }
+
+  const play = () => {
+    NativeModules.Counter.changePlayerPlayback("PLAY")
+  }
+
+  const pause = () => {
+    NativeModules.Counter.changePlayerPlayback("PAUSE")
+  }
+
+  const changePlayerState = (playbackRate) => {
+    NativeModules.Counter.changePlayerState(
+      playbackRate,
+      "",
+      ""
+    )
   }
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Poc Page</Text>
-      <Button title="Increment" onPress={() => NativeModules.Counter.increment()} />
-      <Button title="Decrement" onPress={() => getDecrementFunc()}/>
-      <Button title="Get Count" onPress={() => getCountFunc()}/>
-      <Button title="Request Auth" onPress={() => requestAuth()}/>
+    <View style={{ justifyContent: 'center', padding: 25, paddingTop: 75 }}>
+      <Text style={{ fontSize: 24 }}>Music Library Page</Text>
+      
+      {songs.map(song => (
+        <Text 
+          key={song.id}
+          style={{
+            padding: 10,
+            borderWidth: 1,
+            margin: 5
+          }}
+          onPress={() => playSong(song.id)}
+        >
+            {song.title} - {song.artistName}
+        </Text>
+      ))}
+
+      <View style={{ marginTop: 15 }}>
+        <Button title="Go Back" onPress={() => changePlayerState(0.75)}/>
+        <Button title="Play" onPress={play}/>
+        <Button title="Pause" onPress={pause}/>
+        <Button title="Go Forward" onPress={() => changePlayerState(1.5)}/>
+
+        {/* <Slider
+          style={{width: 200, height: 40}}
+          minimumValue={0}
+          maximumValue={1}
+          minimumTrackTintColor="#FFFFFF"
+          maximumTrackTintColor="#000000"
+        /> */}
+      </View>
     </View>
   );
 }
