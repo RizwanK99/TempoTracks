916785f5cbfbe3b71e9d42021fe3480b9dad26b9
Sterlyn <sterlyntang31@gmail.com>
2024-02-05 13:24:10 -0500

    Adding theme to setup colors etc

--- iOS-mobile/TempoTracks/src/pages/UserPreferenceWorkoutPage.js
+++ iOS-mobile/TempoTracks/src/pages/UserPreferenceWorkoutPage.js
@@ -9,8 +9,11 @@ import {
 } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 import PageHeading from "../components/Workouts/PageHeading";
+import { Button } from "@mui/material";
+import { useTheme } from "@emotion/react";
 
 const UserPreferenceWorkoutPage = ({ navigation }) => {
+  const theme = useTheme();
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "#000" }}>
       <TouchableWithoutFeedback onPress={() => navigation.navigate("Workouts")}>
@@ -30,6 +33,7 @@ const UserPreferenceWorkoutPage = ({ navigation }) => {
       </TouchableWithoutFeedback>
       <View style={{ marginTop: 30, marginLeft: 8 }}>
         <PageHeading title={"A Workout For You"} />
+        <Text style={{ color: theme.colors.primary }}>Here is theme test</Text>
       </View>
     </SafeAreaView>
   );
