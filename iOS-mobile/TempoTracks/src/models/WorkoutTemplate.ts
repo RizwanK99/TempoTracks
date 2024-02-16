55e720fb1cca06a67bf088357f3566dda6af1498
Sterlyn <sterlyntang31@gmail.com>
2024-02-16 17:02:27 -0500

    Delete WorkoutTemplate.ts

--- iOS-mobile/TempoTracks/src/models/WorkoutTemplate.ts
+++
@@ -1,9 +0,0 @@
-export type WorkoutTemplate = {
-  description: string | null;
-  expected_duration: number | null;
-  expected_distance: number | null;
-  name: string;
-  playlist_id: number | null;
-  type: string;
-  user_id: number | null;
-};
