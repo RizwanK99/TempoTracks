a7f30537fc8358aad1d21d8c7295475f7c1bda24
Sterlyn <sterlyntang31@gmail.com>
2024-02-19 20:30:57 -0500

    added swipeable caro for workout in progress

--- iOS-mobile/TempoTracks/src/pages/WorkoutInProgressPage.tsx
+++ iOS-mobile/TempoTracks/src/pages/WorkoutInProgressPage.tsx
@@ -1,191 +1,70 @@
-import React, { useState, useMemo } from "react";
+import React, { useState } from "react";
 import { SafeAreaView, Text, View } from "react-native";
-import { useTheme, IconButton } from "react-native-paper";
-import { Button as PaperButton } from "react-native-paper";
-import {
-  usePauseWorkout,
-  useResumeWorkout,
-  useEndWorkout,
-} from "../api/WorkoutsNew.ts";
-import { useStopwatch } from "react-timer-hook";
+import { useTheme } from "react-native-paper";
+import { WorkoutInProgressDetails } from "../components/Workouts/WorkoutInProgressDetails";
+import { WorkoutInProgressSongPlayer } from "../components/Workouts/WorkoutInProgressSongPlayer";
+import type { ICarouselInstance } from "react-native-reanimated-carousel";
+import Carousel from "react-native-reanimated-carousel";
+import { useWindowDimensions, Dimensions } from "react-native";
+import { useSharedValue } from "react-native-reanimated";
 
-interface TimeProps {
-  value: string;
-  unit: string;
-  showColon?: boolean;
-}
-
-const Time: React.FC<TimeProps> = ({ value, unit, showColon = true }) => {
-  const theme = useTheme();
-  return (
-    <Text style={{ color: theme.colors.primary, fontSize: 52 }} key={unit}>
-      {showColon && ":"}
-      {value < 10 ? `0${value}` : value}
-    </Text>
-  );
-};
+const { width } = Dimensions.get("window");
 
 const WorkoutInProgressPage = ({ navigation, route }) => {
   const theme = useTheme();
   const { workoutId } = route.params;
-  const { mutate: pauseWorkout } = usePauseWorkout();
-  const { mutate: resumeWorkout } = useResumeWorkout();
-  const { mutate: endWorkout } = useEndWorkout();
-  const [paused, setPaused] = useState<boolean>(false);
-  const {
-    totalSeconds,
-    seconds,
-    minutes,
-    hours,
-    isRunning,
-    start,
-    pause,
-    reset,
-  } = useStopwatch({ autoStart: true });
+  const [activeIndex, setActiveIndex] = useState<boolean>(false);
 
-  // Needed since calling reset after mutation
-  const duration = useMemo(() => {
-    return totalSeconds;
-  }, [totalSeconds]);
+  const windowWidth = useWindowDimensions().width;
+  const scrollOffsetValue = useSharedValue<number>(0);
+  const [isFast, setIsFast] = React.useState(false);
+  const [isPagingEnabled, setIsPagingEnabled] = React.useState(true);
+  const ref = React.useRef<ICarouselInstance>(null);
+  const [data, setData] = React.useState([...new Array(2).keys()]);
 
-  const handleWorkoutEnd = () => {
-    endWorkout({ workoutId, duration });
-    reset();
-    navigation.navigate("WorkoutSummaryPage", {
-      workoutId: workoutId,
-    });
-  };
+  const baseOptions = {
+    vertical: false,
+    width: windowWidth,
+    height: "100%",
+  } as const;
+
   return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-        padding: 24,
-        gap: 8,
-        backgroundColor: theme.colors.background,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text style={{ color: theme.colors.text, fontSize: 24 }}>Total Time</Text>
-      <View
-        style={{
-          flexDirection: "row",
-          padding: 8,
-          alignSelf: "center",
+    <SafeAreaView style={{ flex: 1, backgroundColor: theme.colors.background }}>
+      <Carousel
+        {...baseOptions}
+        loop
+        enabled // Default is true, just for demo
+        ref={ref}
+        defaultScrollOffsetValue={scrollOffsetValue}
+        testID={"workoutInProgess"}
+        style={{ width: "100%" }}
+        autoPlay={false}
+        autoPlayInterval={isFast ? 100 : 2000}
+        data={data}
+        onScrollStart={() => {
+          console.log("===1");
         }}
-      >
-        <Time unit="hours" value={hours} showColon={false} />
-        <Time unit="minutes" value={minutes} />
-        <Time unit="seconds" value={seconds} />
-      </View>
-      <View style={{ flexDirection: "row", gap: 32 }}>
-        <View
-          style={{
-            flexDirection: "column",
-            width: "28%",
-            alignItems: "center",
-            gap: 8,
-          }}
-        >
-          <PaperButton
-            style={{
-              borderRadius: 24,
-              width: "100%",
-              height: 48,
-              backgroundColor: theme.colors.redPrimaryForeground,
-              opacity: 0.8,
-              border: "none",
-            }}
-            textColor={theme.colors.redPrimary}
-            labelStyle={{
-              fontSize: 36,
-              justifyContent: "center",
-              alignItems: "center",
-              alignContent: "center",
-              marginLeft: 4,
-              marginTop: 24,
-            }}
-            contentStyle={{ color: theme.colors.text }}
-            icon="stop"
-            onPress={() => {
-              handleWorkoutEnd();
-            }}
-          />
-          <Text style={{ color: theme.colors.text, fontSize: 22 }}>Stop</Text>
-        </View>
-        <View
-          style={{
-            flexDirection: "column",
-            width: "28%",
-            alignItems: "center",
-            gap: 8,
-          }}
-        >
-          {!paused ? (
-            <>
-              <PaperButton
-                style={{
-                  borderRadius: 24,
-                  width: "100%",
-                  height: 48,
-                  backgroundColor: theme.colors.primaryForeground,
-                  border: "none",
-                }}
-                textColor={theme.colors.primary}
-                labelStyle={{
-                  fontSize: 36,
-                  justifyContent: "center",
-                  alignItems: "center",
-                  alignContent: "center",
-                  marginLeft: 4,
-                  marginTop: 24,
-                }}
-                contentStyle={{ color: theme.colors.text }}
-                icon="pause"
-                onPress={() => {
-                  pauseWorkout(workoutId);
-                  pause();
-                  setPaused(true);
-                }}
+        onScrollEnd={() => {
+          console.log("===2");
+        }}
+        onConfigurePanGesture={(g) => g.enabled(false)}
+        pagingEnabled={isPagingEnabled}
+        onSnapToItem={(index) => console.log("current index:", index)}
+        renderItem={({ index }) =>
+          index === 0 ? (
+            <SafeAreaView style={{ flex: 1 }}>
+              <WorkoutInProgressDetails
+                workoutId={workoutId}
+                navigation={navigation}
               />
-              <Text style={{ color: theme.colors.text, fontSize: 22 }}>
-                Pause
-              </Text>
-            </>
+            </SafeAreaView>
           ) : (
-            <>
-              <PaperButton
-                style={{
-                  borderRadius: 24,
-                  width: "100%",
-                  height: 48,
-                  backgroundColor: theme.colors.primaryForeground,
-                  border: "none",
-                }}
-                textColor={theme.colors.primary}
-                labelStyle={{
-                  fontSize: 36,
-                  justifyContent: "center",
-                  alignItems: "center",
-                  alignContent: "center",
-                  marginLeft: 4,
-                  marginTop: 24,
-                }}
-                contentStyle={{ color: theme.colors.text }}
-                icon="play"
-                onPress={() => {
-                  resumeWorkout(workoutId);
-                  start();
-                  setPaused(false);
-                }}
-              />
-              <Text style={{ color: theme.colors.text, fontSize: 22 }}>
-                Resume
-              </Text>
-            </>
-          )}
-        </View>
-      </View>
+            <SafeAreaView style={{ flex: 1 }}>
+              <WorkoutInProgressSongPlayer />
+            </SafeAreaView>
+          )
+        }
+      />
     </SafeAreaView>
   );
 };
