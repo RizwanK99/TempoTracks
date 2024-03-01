8853d8d9d438d3dbe867b2542dd88383dc04dae1
Rizwan i7 <rizwankassamali1@gmail.com>
2024-03-01 16:02:44 -0500

    auth

--- iOS-mobile/TempoTracks/src/api/User.ts
+++ iOS-mobile/TempoTracks/src/api/User.ts
@@ -1,4 +1,23 @@
+import 'react-native-url-polyfill/auto'
+import { createClient } from "@supabase/supabase-js";
+
 // TODO: this should actually use supabase client and be strongly typed
+const supabase = createClient("https://kbgiqwyohojnejjlkwae.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtiZ2lxd3lvaG9qbmVqamxrd2FlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODY2NzQyOTMsImV4cCI6MjAwMjI1MDI5M30.FSCqDJdjvsbpwNp2IJr1LFjtnUkXI-gzwKjJnrkc8JM");
+
+async function signUpNewUser(email, password) {
+  const { data, error } = await supabase.auth.signUp({
+    email: email,
+    password: password,
+  })
+
+  console.log(error)
+
+  if (data){
+    console.log("from auth")
+    console.log(data.user?.id)
+    return data.user?.id
+  }
+}
 
 async function createUser(
   username,
@@ -8,57 +27,54 @@ async function createUser(
   phoneNumber,
   password
 ) {
+
+  let user_id = await signUpNewUser(email, password);
+  console.log("pub " + user_id)
   const payload = {
+    user_id: user_id,
     first_name: firstName,
     last_name: lastName,
     username: username,
     email: email,
     phone_number: phoneNumber,
-    password: password,
   };
 
-  let headers = new Headers();
-  headers.append("Content-Type", "application/json");
-  headers.append("Accept", "application/json");
+  const result = await supabase.from('users').insert(payload).select().single();
 
-  let response = await fetch(
-    "https://kbgiqwyohojnejjlkwae.supabase.co/functions/v1/create-user",
-    {
-      method: "POST",
-      headers: headers,
-      body: JSON.stringify({
-        payload: payload,
-      }),
-    }
-  );
-
-  const data = await response.json();
-  console.log(data);
-  return data;
+  console.log("from create user")
+  console.log(result);
+  return result.data;
 }
 
-async function userLogIn(username, password) {
-  const payload = {
-    username: username,
+async function signInWithEmail(email, password) {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email: email,
     password: password,
-  };
-  let headers = new Headers();
-  headers.append("Content-Type", "application/json");
-  headers.append("Accept", "application/json");
+  })
 
-  let response = await fetch(
-    "https://kbgiqwyohojnejjlkwae.supabase.co/functions/v1/user-login",
-    {
-      method: "POST",
-      headers: headers,
-      body: JSON.stringify({
-        payload: payload,
-      }),
-    }
-  );
-  const data = await response.json();
-  console.log(data);
-  return data;
+  console.log("from auth")
+  console.log(data)
+  console.log(error)
+  return data.user?.id;
+}
+
+async function userLogIn(email, password) {
+
+  console.log("sign in user")
+  console.log(email + " " + password)
+
+  const user_id = await signInWithEmail(email, password);
+
+  console.log("user id")
+  console.log(user_id)
+
+  const result = await supabase.from('users').select().eq('user_id', user_id).single();
+
+  console.log("sign in user")
+  console.log(result);
+  return result.data.user_id;
+
+ 
 }
 
 export { createUser, userLogIn };
